1c8e2845fa610ae290f006f8f06f8497a35c6443
Ww-LUCK-wW <rafaelcarvalho121929@gmail.com>
2025-07-31 03:57:06 -0300

    Update index.js

--- monsuresu/index.js
+++ monsuresu/index.js
@@ -1,5 +1,5 @@
 module.exports = class MonsureScan extends Madara {
   constructor() {
-    super("Monsure Scan", "https://monsuresu.com", "pt-BR");
+    super("Monsure Scan", "https://monsuresu.com/", "pt-BR");
   }
 };
